611f049b015f4a6dcef98463d46c6457e12f9540
Jaime Abbariao <jaime@gooroo.com>
2017-02-11 01:43:53 -0500

    Add files via upload

--- Homework1/hw1_p1.py
+++ Homework1/hw1_p1.py
@@ -17,8 +17,8 @@ I = 1000 # number of samples
 dt = T/I # increments of steps
 discount_factor = np.exp(-r * T) # discount factor for option pricing
 
-lookback_price = {} # dictionary for prices by the rho value
-asian_price = {} # dictionary for prices by the rho value
+op1_price = {} # dictionary for prices by the rho value
+op2_price = {} # dictionary for prices by the rho value
 
 # generate a list of rho values from 0 to 1 by steps of 0.1
 rhos = [x for x in np.arange(0, 1, 0.1)] + [0.99999999]
@@ -46,18 +46,18 @@ def simulatePath(drift, rand, norm):
 	return s0 * np.exp(drift + rand*norm)
 
 '''
-This function returns the payoff of the lookback option
+This function returns the payoff of the op1 option
 in 1(a) divided by the number of simulations.
 '''
-def lookbackPayoff(s0, s1, s2):
+def op1Payoff(s0, s1, s2):
 	lp = max(s0.max(), s1.max(), s2.max()) - 110
 	return lp/M if lp > 0 else 0
 
 '''
-This function returns the payoff of the asian option
+This function returns the payoff of the op2 option
 in 1(b) divided by the number of simulations
 '''
-def asianPayoff(s0, s1, s2):
+def op2Payoff(s0, s1, s2):
 	ap = np.average([s0[len(s0)-1], s1[len(s1)-1], s2[len(s2)-1]])-110
 	return ap/M if ap > 0 else 0
 
@@ -69,7 +69,7 @@ for rho in rhos: # we loop through the values of rho
 	'''
 	cho_mat = np.linalg.cholesky([[1, rho, rho], [rho, 1, rho], [rho, rho, 1]])
 	
-	lookback_payoff_sum = asian_payoff_sum = 0 # initializing the running sum
+	op1_payoff_sum = op2_payoff_sum = 0 # initializing the running sum
 	
 	
 	for i in range(0, M): # loop through simulations
@@ -94,30 +94,30 @@ for rho in rhos: # we loop through the values of rho
 		sz = simulatePath(zdrift, zrand, znorm)
 		
 		
-		lookback_payoff_sum += lookbackPayoff(sx, sy, sz) # collect the sum of the payoffs 
+		op1_payoff_sum += op1Payoff(sx, sy, sz) # collect the sum of the payoffs 
 		
-		asian_payoff_sum += asianPayoff(sx, sy, sz) # collect the sum of the payoffs
+		op2_payoff_sum += op2Payoff(sx, sy, sz) # collect the sum of the payoffs
 	
 	'''
 	The price of the option can be calculated discounting the average payoff
 	After that, we enter the rho and price into the dictionary as a key-value pair
 	'''
-	lookback_price[rho] = lookback_payoff_sum * discount_factor
-	asian_price[rho] = asian_payoff_sum *discount_factor
+	op1_price[rho] = op1_payoff_sum * discount_factor
+	op2_price[rho] = op2_payoff_sum *discount_factor
 
 
-print('Lookback Option: ')
-print([lookback_price[x] for x in rhos])
+print('op1 Option: ')
+print([op1_price[x] for x in rhos])
 print('\n')
-print('Asian Option: ')
-print([asian_price[x] for x in rhos])
+print('op2 Option: ')
+print([op2_price[x] for x in rhos])
 
-plt.scatter(list(lookback_price.keys()), list(lookback_price.values()), label='Lookback')
+plt.scatter(list(op1_price.keys()), list(op1_price.values()), label='op1')
 plt.title('1(a) and 1(b) Option')
 plt.xlabel('Rho')
 plt.ylabel('Price of the Option')
 plt.xlim([-0.1, 1.1])
-plt.scatter(list(asian_price.keys()), list(asian_price.values()), color='red', label='Asian')
+plt.scatter(list(op2_price.keys()), list(op2_price.values()), color='red', label='op2')
 plt.legend()
 plt.show()
 
